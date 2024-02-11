4b4463535369b8da262f3bb7adfc7ba88fc2274c
Marvin Gajek <Marvin.Gajek1@web.de>
2024-02-11 20:37:47 +0100

    [doc] - add comment on stb utilization

--- apps/rotating-textured-cube/include/rotating-textured-cube.h
+++ apps/rotating-textured-cube/include/rotating-textured-cube.h
@@ -38,7 +38,7 @@
 #include <glm/gtc/type_ptr.hpp>
 
 // To prevent linking errors, refrain from relocating the #define STB_IMAGE_IMPLEMENTATION macro to this location. It is restricted to a single usage.
-// Tinygltf internally utilizes functions from the stb_image* library and requires macros to be set in order to use those. 
+// Tinygltf internally utilizes functions from the stb_image header and requires macros to be set in order to use those. 
 // See: https://github.com/syoyo/tinygltf/blob/release/stb_image.h
 #include <stb_image.h>
 
