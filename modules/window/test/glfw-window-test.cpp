59c4e8a545d5c14f333069e8310eb3db1ac49a49
Marvin Gajek <Marvin.Gajek1@web.de>
2024-02-18 18:38:19 +0100

    [improvement, qa] - implement missing tests for GetWindowSize() and GetWindow() methods

--- modules/window/test/glfw-window-test.cpp
+++ modules/window/test/glfw-window-test.cpp
@@ -9,10 +9,7 @@ namespace Mgtt::Window::Test {
         static std::unique_ptr<Mgtt::Window::GlfwWindow> glfwWindow;
     protected:
         void SetUp() override {}
-        void TearDown() override {
-            this->glfwWindow->~GlfwWindow();
-        }
-
+        void TearDown() override {}
         static void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
         static void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
         static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
@@ -24,6 +21,19 @@ namespace Mgtt::Window::Test {
     void GlfwWindowTest::FramebufferSizeCallback(GLFWwindow* window, int width, int height) {}
     void GlfwWindowTest::ScrollCallback(GLFWwindow* window, double xoffset, double yoffset) {}
     void GlfwWindowTest::KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {}
+
+    TEST_F(GlfwWindowTest, GetWindow) {
+        RecordProperty("Test Description", "Checks if GetWindow method returns a non-null GLFW window pointer");
+        RecordProperty("Expected Result", "No exception is thrown.");
+
+        try {
+            GLFWwindow* window = GlfwWindowTest::glfwWindow->GetWindow();
+            EXPECT_NE(window, nullptr) << "The GetWindow method should return a non-null pointer.";
+        }
+        catch (...) {
+            FAIL() << "An exception has been thrown" << std::endl;
+        }
+    }
 
     TEST_F(GlfwWindowTest, SetFramebufferSizeCallback) {
         RecordProperty("Test Description", "Checks if SetFramebufferSizeCallback method works");
@@ -70,6 +80,21 @@ namespace Mgtt::Window::Test {
 
         try {
             GlfwWindowTest::glfwWindow->SwapBuffersAndPollEvents();
+        }
+        catch (...) {
+            FAIL() << "An exception has been thrown" << std::endl;
+        }
+    }
+
+    // Test case for the GetWindowSize method
+    TEST_F(GlfwWindowTest, GetWindowSize) {
+        RecordProperty("Test Description", "Checks if GetWindowSize method returns a valid width and height tuple");
+        RecordProperty("Expected Result", "No exception is thrown.");
+
+        try {
+            auto [width, height] = GlfwWindowTest::glfwWindow->GetWindowSize();
+            EXPECT_GE(width, 800) << "The width should be a non-negative value.";
+            EXPECT_GE(height, 600) << "The height should be a non-negative value.";
         }
         catch (...) {
             FAIL() << "An exception has been thrown" << std::endl;
