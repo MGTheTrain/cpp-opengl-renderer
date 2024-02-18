69757e338a1797b2857d020502e0391c21ca0c70
Marvin Gajek <Marvin.Gajek1@web.de>
2024-02-18 18:30:21 +0100

    [improvement, qa] - create seperate entrypoint.cpp file, add namespace in test, make public member attribute static in test

--- modules/window/test/glfw-window-test.cpp
+++ modules/window/test/glfw-window-test.cpp
@@ -3,91 +3,89 @@
 #include <glfw-window.h>
 
 
-class GlfwWindowTest : public ::testing::Test {
-public:
-    std::unique_ptr<Mgtt::Window::GlfwWindow> glfwWindow;
-protected:
-    void SetUp() override {
-        this->glfwWindow = std::make_unique<Mgtt::Window::GlfwWindow>("test-window", 800, 600);
+namespace Mgtt::Window::Test {
+    class GlfwWindowTest : public ::testing::Test {
+    public:
+        static std::unique_ptr<Mgtt::Window::GlfwWindow> glfwWindow;
+    protected:
+        void SetUp() override {}
+        void TearDown() override {
+            this->glfwWindow->~GlfwWindow();
+        }
+
+        static void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
+        static void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+        static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+    };
+
+    std::unique_ptr<Mgtt::Window::GlfwWindow> GlfwWindowTest::glfwWindow =
+        std::make_unique<Mgtt::Window::GlfwWindow>("test-window", 800, 600);
+
+    void GlfwWindowTest::FramebufferSizeCallback(GLFWwindow* window, int width, int height) {}
+    void GlfwWindowTest::ScrollCallback(GLFWwindow* window, double xoffset, double yoffset) {}
+    void GlfwWindowTest::KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {}
+
+    TEST_F(GlfwWindowTest, SetFramebufferSizeCallback) {
+        RecordProperty("Test Description", "Checks if SetFramebufferSizeCallback method works");
+        RecordProperty("Expected Result", "No exception is thrown.");
+
+        try {
+            GlfwWindowTest::glfwWindow->SetFramebufferSizeCallback(GlfwWindowTest::FramebufferSizeCallback);
+        }
+        catch (...) {
+            FAIL() << "An exception has been thrown" << std::endl;
+        }
     }
-    void TearDown() override {
-        this->glfwWindow->~GlfwWindow();
+
+    // Test case for the SetScrollCallback method
+    TEST_F(GlfwWindowTest, SetScrollCallback) {
+        RecordProperty("Test Description", "Checks if SetScrollCallback method works");
+        RecordProperty("Expected Result", "No exception is thrown.");
+
+        try {
+            GlfwWindowTest::glfwWindow->SetScrollCallback(GlfwWindowTest::ScrollCallback);
+        }
+        catch (...) {
+            FAIL() << "An exception has been thrown" << std::endl;
+        }
     }
 
-    static void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
-    static void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
-    static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
-};
+    // Test case for the SetKeyCallback method
+    TEST_F(GlfwWindowTest, SetKeyCallback) {
+        RecordProperty("Test Description", "Checks if SetKeyCallback method works");
+        RecordProperty("Expected Result", "No exception is thrown.");
 
-void GlfwWindowTest::FramebufferSizeCallback(GLFWwindow* window, int width, int height) {}
-void GlfwWindowTest::ScrollCallback(GLFWwindow* window, double xoffset, double yoffset) {}
-void GlfwWindowTest::KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {}
-
-TEST_F(GlfwWindowTest, SetFramebufferSizeCallback) {
-    RecordProperty("Test Description", "Checks if SetFramebufferSizeCallback method works");
-    RecordProperty("Expected Result", "No exception is thrown.");
-
-    try {
-        glfwWindow->SetFramebufferSizeCallback(GlfwWindowTest::FramebufferSizeCallback);
+        try {
+            GlfwWindowTest::glfwWindow->SetKeyCallback(GlfwWindowTest::KeyCallback);
+        }
+        catch (...) {
+            FAIL() << "An exception has been thrown" << std::endl;
+        }
     }
-    catch (...) {
-        FAIL() << "An exception has been thrown" << std::endl;
-    }
-}
 
-// Test case for the SetScrollCallback method
-TEST_F(GlfwWindowTest, SetScrollCallback) {
-    RecordProperty("Test Description", "Checks if SetScrollCallback method works");
-    RecordProperty("Expected Result", "No exception is thrown.");
+    // Test case for the SwapBuffersAndPollEvents method
+    TEST_F(GlfwWindowTest, SwapBuffersAndPollEvents) {
+        RecordProperty("Test Description", "Checks if SwapBuffersAndPollEvents method works");
+        RecordProperty("Expected Result", "No exception is thrown.");
 
-    try {
-        glfwWindow->SetScrollCallback(GlfwWindowTest::ScrollCallback);
+        try {
+            GlfwWindowTest::glfwWindow->SwapBuffersAndPollEvents();
+        }
+        catch (...) {
+            FAIL() << "An exception has been thrown" << std::endl;
+        }
     }
-    catch (...) {
-        FAIL() << "An exception has been thrown" << std::endl;
-    }
-}
 
-// Test case for the SetKeyCallback method
-TEST_F(GlfwWindowTest, SetKeyCallback) {
-    RecordProperty("Test Description", "Checks if SetKeyCallback method works");
-    RecordProperty("Expected Result", "No exception is thrown.");
+    // Test case for the WindowShouldClose method
+    TEST_F(GlfwWindowTest, WindowShouldClose) {
+        RecordProperty("Test Description", "Checks if WindowShouldClose method works");
+        RecordProperty("Expected Result", "No exception is thrown.");
 
-    try {
-        glfwWindow->SetKeyCallback(GlfwWindowTest::KeyCallback);
+        try {
+            GlfwWindowTest::glfwWindow->SwapBuffersAndPollEvents();
+        }
+        catch (...) {
+            FAIL() << "An exception has been thrown" << std::endl;
+        }
     }
-    catch (...) {
-        FAIL() << "An exception has been thrown" << std::endl;
-    }
-}
-
-// Test case for the SwapBuffersAndPollEvents method
-TEST_F(GlfwWindowTest, SwapBuffersAndPollEvents) {
-    RecordProperty("Test Description", "Checks if SwapBuffersAndPollEvents method works");
-    RecordProperty("Expected Result", "No exception is thrown.");
-
-    try {
-        glfwWindow->SwapBuffersAndPollEvents();
-    }
-    catch (...) {
-        FAIL() << "An exception has been thrown" << std::endl;
-    }
-}
-
-// Test case for the WindowShouldClose method
-TEST_F(GlfwWindowTest, WindowShouldClose) {
-    RecordProperty("Test Description", "Checks if WindowShouldClose method works");
-    RecordProperty("Expected Result", "No exception is thrown.");
-
-    try {
-        glfwWindow->SwapBuffersAndPollEvents();
-    }
-    catch (...) {
-        FAIL() << "An exception has been thrown" << std::endl;
-    }
-}
-
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }
