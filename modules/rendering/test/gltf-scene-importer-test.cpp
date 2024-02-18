55375f0129f1cda851aa8649ad46cf04a564db58
Marvin Gajek <Marvin.Gajek1@web.de>
2024-02-18 19:39:36 +0100

    [qa, improvement] - add record properties

--- modules/rendering/test/gltf-scene-importer-test.cpp
+++ modules/rendering/test/gltf-scene-importer-test.cpp
@@ -25,6 +25,9 @@ namespace Mgtt::Rendering::Test {
     std::unique_ptr<Mgtt::Rendering::GltfSceneImporter> GltfSceneImporterTest::gltfSceneImporter = std::make_unique<Mgtt::Rendering::GltfSceneImporter>();
     
     TEST_F(GltfSceneImporterTest, LoadInvalidGltfFile) {
+         RecordProperty("Test Description", "Checks if LoadInvalidGltfFile handles invalid Gltf file");
+        RecordProperty("Expected Result", "std::runtime_error is caught");
+
         try {
             GltfSceneImporterTest::gltfSceneImporter->Load(GltfSceneImporterTest::mgttScene, "assets/scenes/water-bottle/None-Existen-WaterBottle.gltf");
         }
@@ -37,10 +40,13 @@ namespace Mgtt::Rendering::Test {
     }
 
     TEST_F(GltfSceneImporterTest, LoadValidGltfFile) {
+        RecordProperty("Test Description", "Checks if LoadValidGltfFile loads a valid Gltf file");
+        RecordProperty("Expected Result", "No exception is thrown");
+
         try {
             std::pair<std::string, std::string> shaderPathes = { "assets/shader/core/pbr.vert", "assets/shader/core/pbr.frag" };
             GltfSceneImporterTest::mgttScene.shader.Compile(shaderPathes);
-            GltfSceneImporterTest::gltfSceneImporter->Load(GltfSceneImporterTest::mgttScene, "assets/scenes/water-bottle/WaterBottle.gltf");
+            GltfSceneImporterTest::gltfSceneImporter->Load(GltfSceneImporterTest::mgttScene, "assets/scenes/water-bottle/WaterBottle.gltf"); 
             EXPECT_EQ("assets/scenes/water-bottle/WaterBottle.gltf", GltfSceneImporterTest::mgttScene.path);
         }
         catch (...) {
@@ -49,6 +55,9 @@ namespace Mgtt::Rendering::Test {
     }
 
     TEST_F(GltfSceneImporterTest, ClearScene) {
+        RecordProperty("Test Description", "Checks if ClearScene clears the scene");
+        RecordProperty("Expected Result", "No exception is thrown");
+        
         try {
             GltfSceneImporterTest::gltfSceneImporter->Clear(this->mgttScene);
         }
