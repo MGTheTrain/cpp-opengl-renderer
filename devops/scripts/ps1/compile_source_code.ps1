f61458f25380c308352dd8621661c3a4991f957b
Marvin Gajek <Marvin.Gajek1@web.de>
2024-02-19 23:32:58 +0100

    [improvement] - update ps1 script for compiling and linking binaries via CMake

--- devops/scripts/ps1/compile_source_code.ps1
+++ devops/scripts/ps1/compile_source_code.ps1
@@ -1,3 +1,13 @@
-cd ../..
-cmake -B build -DBUILD_LIB=ON -DBUILD_TEST=ON  -DCMAKE_TOOLCHAIN_FILE="D:\c++ repos\dependencies\vcpkg\scripts\buildsystems\vcpkg.cmake"  .
+param(
+    [string]$CMakeToolchainFile
+)
+
+if ($CMakeToolchainFile -eq $null) {
+    $CMakeToolchainFile = Read-Host "Enter the path to CMAKE_TOOLCHAIN_FILE" 
+}
+$currentDir = Get-Location
+Set-Location -Path "$PSScriptRoot/../../.."
+cmake -B build -DBUILD_LIB=ON -DBUILD_TEST=ON -DBUILD_APP=ON -DCMAKE_TOOLCHAIN_FILE="$CMakeToolchainFile" .
 cmake --build build
+Set-Location -Path $currentDir
+echo ".\compile_source_code.ps1 -CMakeToolchainFile `"$CMakeToolchainFile`""
