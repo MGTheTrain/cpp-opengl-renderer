11b5d9b2b7ec5eb610c1198b33dbb54df3b3c84b
Marvin Gajek <Marvin.Gajek1@web.de>
2024-02-21 20:41:04 +0100

    [improvement] - update ps1 script in that ctest's can be executed with a flag

--- devops/scripts/ps1/compile_source_code.ps1
+++ devops/scripts/ps1/compile_source_code.ps1
@@ -1,13 +1,22 @@
 param(
-    [string]$CMakeToolchainFile
+    [string]$CMakeToolchainFile,
+    [switch]$RunTests
 )
 
 if ($CMakeToolchainFile -eq $null) {
     $CMakeToolchainFile = Read-Host "Enter the path to CMAKE_TOOLCHAIN_FILE" 
 }
+
 $currentDir = Get-Location
 Set-Location -Path "$PSScriptRoot/../../.."
 cmake -B build -DBUILD_LIB=ON -DBUILD_TEST=ON -DBUILD_APP=ON -DCMAKE_TOOLCHAIN_FILE="$CMakeToolchainFile" .
+
 cmake --build build
-Set-Location -Path $currentDir
-echo "Example: .\compile_source_code.ps1 -CMakeToolchainFile `"D:\c++ repos\dependencies\vcpkg\scripts\buildsystems\vcpkg.cmake`""
+
+if ($RunTests) {
+    Set-Location -Path "build"
+    ctest
+    Set-Location -Path $currentDir
+}
+
+echo "Example: .\compile_source_code.ps1 -CMakeToolchainFile `"D:\c++ repos\dependencies\vcpkg\scripts\buildsystems\vcpkg.cmake`" -RunTests"
