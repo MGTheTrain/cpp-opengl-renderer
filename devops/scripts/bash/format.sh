d802f17ad266c4af1bbd66542bf05b1cb9fb2cb8
Marvin Gajek <Marvin.Gajek1@web.de>
2024-02-23 19:11:38 +0100

    update script in that clang-format is exececuted from the root project dir

--- devops/scripts/bash/format.sh
+++ devops/scripts/bash/format.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-find ../** -name '*.h' -o -name '*.cpp' | xargs clang-format -i
+SCRIPT_DIR=$(dirname "$BASH_SOURCE")
+ROOT_PROJECT_DIR=$SCRIPT_DIR/../..
+
+cd $ROOT_PROJECT_DIR
+
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+NC='\033[0m' 
+
+echo "#####################################################################################################"
+echo -e "$BLUE INFO: $NC About to lint cpp files"
+find ./** -name '*.h' -o -name '*.cpp' | xargs clang-format -i
+
+cd -
