1ba509541e76d57b8da0b1fcc34fd7bb3b85d93e
Marvin Gajek <Marvin.Gajek1@web.de>
2024-03-17 04:48:34 +0100

    [improvement] - Install emscripten SDK in dev containers

--- .devcontainer/ubuntu20.04-amd64/scripts/install-dependencies.sh
+++ .devcontainer/ubuntu20.04-amd64/scripts/install-dependencies.sh
@@ -5,3 +5,9 @@ apt-get install -y libxmu-dev libxi-dev libgl-dev \
         libxinerama-dev libxcursor-dev xorg-dev \
         libglu1-mesa-dev pkg-config python3 python3-pip
 pip3 install clang-format cpplint
+
+cd tmp
+git clone https://github.com/emscripten-core/emsdk.git
+cd emsdk
+./emsdk install latest-upstream
+./emsdk activate latest-upstream
