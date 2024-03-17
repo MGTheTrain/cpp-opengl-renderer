4c3ca8273495da2cfe839c344a6f159efcfa7f4f
Marvin Gajek <Marvin.Gajek1@web.de>
2024-03-17 04:59:50 +0100

    [fix] - navigate to correct folder

--- .devcontainer/debian11-amd64/scripts/install-dependencies.sh
+++ .devcontainer/debian11-amd64/scripts/install-dependencies.sh
@@ -6,7 +6,7 @@ apt-get install -y libxmu-dev libxi-dev libgl-dev \
         libglu1-mesa-dev pkg-config python3 python3-pip
 pip3 install clang-format cpplint
 
-cd tmp
+cd /tmp/
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
 ./emsdk install latest-upstream
