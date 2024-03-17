698a2e606c20b6af6de9ddeb9a706cdd78279260
Marvin Gajek <Marvin.Gajek1@web.de>
2024-03-17 09:21:18 +0100

    [improvement] - enable desktop or web builds in scripts

--- devops/scripts/bash/compile_source_code.sh
+++ devops/scripts/bash/compile_source_code.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 NoTests=true
+NoWebBuild=true
 
 while [ "$#" -gt 0 ]; do
   case "$1" in
@@ -10,16 +11,26 @@ while [ "$#" -gt 0 ]; do
       CMakeToolchainFile="$2"
       shift 2
       ;;
+    -EmscriptenCMakeFile)
+      EmscriptenCMakeFile="$3"
+      shift 2
+      ;;
     -RunTests)
       NoTests=false
       shift
       ;;
+    -WebBuild)
+      NoWebBuild=false
+      shift
+      ;;
     -h|--help)
-      echo "Usage: $0 [-CMakeToolchainFile <path>] [-RunTests] [-h|--help]"
+      echo "Usage: $0 [-CMakeToolchainFile <path>] [-EmscriptenCMakeFile <path>] [-WebBuild] [-RunTests] [-h|--help]"
       echo "Options:"
-      echo "  -CMakeToolchainFile   Path to CMAKE_TOOLCHAIN_FILE"
-      echo "  -RunTests              Do not run tests"
-      echo "  -h, --help            Display this help message"
+      echo "  -CMakeToolchainFile          Path to CMAKE_TOOLCHAIN_FILE"
+      echo "  -EmscriptenCMakeFile <Path>  Path to EMSCRIPTEN_CMAKE_FILE"
+      echo "  -RunTests                    Run tests after building (optional)"
+      echo "  -WebBuild                    Build for web (with emscripten) instead of Desktop (optional)"
+      echo "  -h, --help                   Display this help message"
       exit 0
       ;;
     *)
@@ -30,7 +41,7 @@ while [ "$#" -gt 0 ]; do
 done
 
 if [ -z "$CMakeToolchainFile" ]; then
-  read -p "Enter the path to CMAKE_TOOLCHAIN_FILE: " CMakeToolchainFile
+  read -p "Enter the path to CMakeToolchainFile, e.g. /d/c++ repos/dependencies/vcpkg/scripts/buildsystems/vcpkg.cmake" CMakeToolchainFile
 fi
 
 currentDir=$(pwd)
@@ -42,7 +53,15 @@ NC='\033[0m'
 echo "#####################################################################################################"
 echo -e "$BLUE INFO: $NC About to compile the source code and link the binaries"
 
-cmake -B build -DBUILD_LIB=ON -DBUILD_TEST=ON -DBUILD_APP=ON -DBUILD_PACKAGE=ON -DCMAKE_TOOLCHAIN_FILE="$CMakeToolchainFile" .
+if [[ "$NoWebBuild" == true ]]; then
+  cmake -B build -DBUILD_LIB=ON -DBUILD_TEST=ON -DBUILD_APP=ON -DBUILD_PACKAGE=ON -DCMAKE_TOOLCHAIN_FILE="$CMakeToolchainFile" .
+else 
+  if [ -z "$EmscriptenCMakeFile" ]; then
+    read -p "Enter the path to EmscriptenCMakeFile, e.g. /d/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" EmscriptenCMakeFile
+  fi
+  cmake -B build -DBUILD_LIB=ON -DBUILD_TEST=ON -DBUILD_APP=ON -DBUILD_PACKAGE=ON -DCMAKE_TOOLCHAIN_FILE="$CMakeToolchainFile" -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE="$EmscriptenCMakeFile" -DVCPKG_TARGET_TRIPLET="wasm32-emscripten" .
+fi
+
 echo -e "$BLUE INFO: $NC Build environment could be successfully generated"
 
 cmake --build build
--- devops/scripts/ps1/compile_source_code.ps1
+++ devops/scripts/ps1/compile_source_code.ps1
@@ -1,14 +1,17 @@
 param(
     [string]$CMakeToolchainFile,
     [switch]$RunTests,
-    [switch]$Help
+    [switch]$Help,
+    [switch]$WebBuild
 )
 
 function Show-Help {
-    echo "Usage: .\compile_source_code.ps1 -CMakeToolchainFile <Path> [-RunTests] [-Help]"
+    echo "Usage: .\compile_source_code.ps1 -CMakeToolchainFile <Path> -EmscriptenCMakeFile <Path> [-RunTests] [-WebBuild] [-Help]"
     echo "  -CMakeToolchainFile <Path>   Path to CMAKE_TOOLCHAIN_FILE"
-    echo "  -RunTests                     Run tests after building (optional)"
-    echo "  -Help                         Display this help message"
+    echo "  -EmscriptenCMakeFile <Path>  Path to EMSCRIPTEN_CMAKE_FILE"
+    echo "  -RunTests                    Run tests after building (optional)"
+    echo "  -WebBuild                    Build for web (with emscripten) instead of Desktop (optional)"
+    echo "  -Help                        Display this help message"
 }
 
 if ($Help) {
@@ -16,13 +19,21 @@ if ($Help) {
     exit
 }
 
-if ($CMakeToolchainFile -eq $null) {
-    $CMakeToolchainFile = Read-Host "Enter the path to CMAKE_TOOLCHAIN_FILE" 
+if (-not $CMakeToolchainFile) {
+    $CMakeToolchainFile = Read-Host "Enter the path to CMakeToolchainFile, e.g. 'D:\c++ repos\dependencies\vcpkg\scripts\buildsystems\vcpkg.cmake'"
 }
 
 $currentDir = Get-Location
 Set-Location -Path "$PSScriptRoot/../../.."
-cmake -B build -DBUILD_LIB=ON -DBUILD_TEST=ON -DBUILD_APP=ON -DBUILD_PACKAGE=ON -DCMAKE_TOOLCHAIN_FILE="$CMakeToolchainFile" .
+
+if ($WebBuild) {
+    if (-not $EmscriptenCMakeFile) {
+        $EmscriptenCMakeFile = Read-Host "Enter the path to EmscriptenCMakeFile, e.g. 'D:\emsdk\upstream\emscripten\cmake\Modules\Platform\Emscripten.cmake'"
+    }
+    cmake -B build -DBUILD_LIB=ON -DBUILD_TEST=ON -DBUILD_APP=ON -DBUILD_PACKAGE=ON -DCMAKE_TOOLCHAIN_FILE="$CMakeToolchainFile" -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE="$EmscriptenCMakeFile" -DVCPKG_TARGET_TRIPLET="wasm32-emscripten" .
+} else {
+    cmake -B build -DBUILD_LIB=ON -DBUILD_TEST=ON -DBUILD_APP=ON -DBUILD_PACKAGE=ON -DCMAKE_TOOLCHAIN_FILE="$CMakeToolchainFile" .
+}
 
 cmake --build build
 
