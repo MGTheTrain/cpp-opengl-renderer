5e1c1ee555c15599a82da358629a5a5c50d26d75
Marvin Gajek <Marvin.Gajek1@web.de>
2024-03-17 15:18:08 +0100

    [improvement] - Add GLSL version 3.0 ES vertex and fragment shader

--- apps/rotating-textured-cube/src/rotating-textured-cube.cpp
+++ apps/rotating-textured-cube/src/rotating-textured-cube.cpp
@@ -62,8 +62,13 @@ Mgtt::Apps::RotatingTexturedCube::RotatingTexturedCube() {
 #endif
   glEnable(GL_DEPTH_TEST);
 
+#ifndef __EMSCRIPTEN__
   std::string vsPath = "assets/shader/core/coordinate.vert";
   std::string fsPath = "assets/shader/core/coordinate.frag";
+#else 
+  std::string vsPath = "assets/shader/es/coordinate.vert";
+  std::string fsPath = "assets/shader/es/coordinate.frag";
+#endif
   std::pair<std::string, std::string> shaderPathes =
       std::make_pair(vsPath, fsPath);
   auto shader = Mgtt::Rendering::OpenGlShader(shaderPathes);
