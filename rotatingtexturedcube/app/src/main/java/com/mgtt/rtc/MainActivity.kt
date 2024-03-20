6bc2e73141d2e731e6dadad3aa4cdaa3ef0be49e
MGTheTrain <Marvin.Gajek1@web.de>
2024-03-20 20:26:14 +0100

    [improvement] - Create Kotlin mobile app including Native C++ support

---
+++ rotatingtexturedcube/app/src/main/java/com/mgtt/rtc/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.mgtt.rtc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.mgtt.rtc.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'rtc' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'rtc' library on application startup.
+        init {
+            System.loadLibrary("rtc")
+        }
+    }
+}
