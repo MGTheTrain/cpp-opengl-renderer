6d76e945cfa3f0856b557b5705099fc4671d4bd2
MGTheTrain <marvin.gajek1@web.de>
2024-03-23 14:58:42 +0100

    [improvement] - create initial RotatingTexturedCube iOS app

---
+++ ios/RotatingTexturedCube/RotatingTexturedCube/RotatingTexturedCubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotatingTexturedCubeApp.swift
+//  RotatingTexturedCube
+//
+//  Created by Marvin Gajek on 23.03.24.
+//
+
+import SwiftUI
+
+@main
+struct RotatingTexturedCubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
